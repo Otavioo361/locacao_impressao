95b384f7ce6c7f106dedd364656e4a6ddcf3b122
Otávio Pereira Tristão <78180132+Otavioo361@users.noreply.github.com>
2023-08-10 08:31:26 -0300

    adição de model

---
+++ Projeto_impressao/src/model/Pessoa_juridica.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Otavio
+ */
+public class Pessoa_juridica extends Pessoa {
+    protected String cnpj;  
+        
+     public Pessoa_juridica(){       
+        this.cnpj = "00000";               
+    }
+    
+}
